ef849e78e1d3b13d175f672e93379a91825a7e29
kislamr <me@jelletimmerman.com>
2015-05-19 20:51:41 +0200

    -updated database

--- scripts/createDatabase.sql
+++ scripts/createDatabase.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE mvcdiary;
+
 CREATE TABLE entry (
 	id BIGSERIAL PRIMARY KEY,
 	title		 VARCHAR(50),
@@ -7,8 +9,13 @@ CREATE TABLE entry (
 	
 INSERT INTO entry(id, title, text, date) VALUES (1, 'Hello world!', 'This is my first post', '08/05/2015');
 INSERT INTO entry(id, title, text, date) VALUES (2, 'Hello world again!', 'This is my second post', '08/05/2015');
-INSERT INTO entry(id, title, text, date) VALUES (3, 'Hello world yet again!', 'This is my third post',
+INSERT INTO entry(id, title, text, date) VALUES ('Hello world yet again!', 'This is my third post',
 
-ALTER TABLE entry ADD COLUMN entry BIGSERIAL PRIMARY KEY;
+CREATE TABLE users (
+	userid		 VARCHAR(20),
+	username     VARCHAR(60),
+	userpassword VARCHAR(10)
+	);
 
-INSERT INTO entry(title, text, date) VALUES ('Hello world!', 'This is my first post', '08/05/2015');
+INSERT INTO users(userid, username, userpassword) VALUES ('jelle', 'Jelle Timmerman', 'qwe');
+INSERT INTO users(userid, username, userpassword) VALUES ('jan', 'Jan Janssen', 'qwe');
--- src/com/kislamr/webapps/util/DBConnectionUtil.java
+++ src/com/kislamr/webapps/util/DBConnectionUtil.java
@@ -4,31 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.servlet.ServletException;
-
 public class DBConnectionUtil {
 	static Connection dbcon = null;
-	static private String loginUrl = "jdbc:postgresql://localhost/postdiarydb";
+	static private String loginUrl = "jdbc:postgresql://localhost/mvcdiary";
 	static private String loginUser = "postgres";
 	static private String loginPasswd = "qwe";
-    
-    public static Connection connect() {
-	// Load the PostgreSQL driver
-    try 
-    {
-          Class.forName("org.postgresql.Driver");
-          dbcon = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
-    }
-    catch (ClassNotFoundException exception)
-    {
-           System.err.println("ClassNotFoundException: " + exception.getMessage());
-    }
-    catch (SQLException ex)
-    {
-           System.err.println("SQLException: " + ex.getMessage());
-    }
-	return dbcon;
-    }
+
+	public static Connection connect() {
+		// Load the PostgreSQL driver
+		try {
+			Class.forName("org.postgresql.Driver");
+			dbcon = DriverManager.getConnection(loginUrl, loginUser,
+					loginPasswd);
+		} catch (ClassNotFoundException exception) {
+			System.err.println("ClassNotFoundException: "
+					+ exception.getMessage());
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
+		return dbcon;
+	}
 
 	public static Connection getDbcon() {
 		return dbcon;
